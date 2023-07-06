d1080d37021dcc24c80b15abf5bfca34dabc6454
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-06 18:22:53 +0200

    añado contenido a dashboard

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+// Detallenotificacion.js
+import Navbar from "../../components/Navbar/NavbarIn";
+import Link from 'next/link'
+const Dashboard = () => {
+  return (
+    <>
+    Check
+    </>
+    );
+};
+
+export default Dashboard;
