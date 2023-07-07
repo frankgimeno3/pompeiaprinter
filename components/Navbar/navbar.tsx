f19826e1b2bc9e5742fa26479f465562907dc9ca
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-07 11:40:29 +0200

    tabla

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -38,7 +38,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-cyan-950 bg-opacity-60 backdrop-filter backdrop-blur-lg flex items-center justify-between px-4 py-2 md:text-xs xl:text-base">
+    <nav className=" bg-cyan-950 bg-opacity-60 backdrop-filter backdrop-blur-lg flex items-center justify-between px-4 py-2 md:text-xs xl:text-base">
       <button
         className={`bg-opacity-25 text-white rounded-full py-2 px-3 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer ${
           selectedButton === "HOME" ? "bg-white bg-opacity-50 text-cyan-950" : ""
--- pages/dashboard/impresion/index.tsx
+++ pages/dashboard/impresion/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../../../components/Navbar/navbar";
-import Link from 'next/link';
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 interface File {
@@ -22,29 +22,29 @@ const Impresion = () => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-      }
+      },
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Credenciales incorrectas");
-      }
-    })
-    .then(response => {
-      console.log(response);
-      setFiles(response);
-    })
-    .catch(error => {
-      console.error("Ha ocurrido un error:", error);
-    });
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Credenciales incorrectas");
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        setFiles(response);
+      })
+      .catch((error) => {
+        console.error("Ha ocurrido un error:", error);
+      });
   }, []);
 
   const handleprint = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("esto te tendría que visualizar el PDF")
-  }
-  
+    console.log("esto te tendría que visualizar el PDF");
+  };
+
   const handledelete = (id: string) => {
     fetch(`http://localhost:5000/files/${id}`, {
       method: "DELETE",
@@ -53,17 +53,17 @@ const Impresion = () => {
       },
       body: JSON.stringify({ id }),
     })
-    .then(response => {
-      if (response.ok) {
-        window.location.reload();
-      } else {
-        throw new Error("Error al eliminar el archivo");
-      }
-    })
-    .catch(error => {
-      console.error("Ha ocurrido un error:", error);
-    });
-  }
+      .then((response) => {
+        if (response.ok) {
+          window.location.reload();
+        } else {
+          throw new Error("Error al eliminar el archivo");
+        }
+      })
+      .catch((error) => {
+        console.error("Ha ocurrido un error:", error);
+      });
+  };
 
   const showDeleteAlert = (id: string) => {
     setSelectedFileId(id);
@@ -80,26 +80,48 @@ const Impresion = () => {
   };
 
   return (
-    <>
+    <div className="h-screen">
       <Navbar />
-      <div className="mx-20 pl-10 mb-5 ">
-        <h1 className="mt-20 text-2xl">Contenido a imprimir</h1>
-        <div className="mr-20 ">
-          {files.map((file, index) => (
-            <div className="flex my-2 flex-row justify-between bg-white align-center pt-1" key={file._id}>
-              <p className="flex mr-20 pl-10 pt-1 mb-5 align-center">{file.content}</p>
-              <div className="my-2 flex-row " key={file._id}>
-                <button
-                  className="rounded bg-gray-100 shadow px-5 py-1 mr-5 text-xs hover:bg-gray-50"
-                  onClick={handleprint}
-                >Visualizar</button>
-                <button
-                  className="rounded bg-gray-100 shadow px-5 py-1 mr-5 text-xs hover:bg-gray-50"
-                  onClick={() => showDeleteAlert(file._id)}
-                >Eliminar</button>
-              </div>
-            </div>
-          ))}
+      <div className="mx-20 pl-10   ">
+        <h1 className="mt-20 text-2xl  font-light">Contenido a imprimir</h1>
+        <div className="mr-20 mt-5 ">
+          
+          <table className="text-xs  border border-gray-300 bg-white">
+            <thead>
+              <tr className="border border-gray-300  ">
+                <th className="py-1.5 px-20 font-medium border border-gray-300">Id</th>
+                <th className="py-1.5 px-20 font-medium border border-gray-300">Fecha</th>
+                <th className="py-1.5 px-20 font-medium border border-gray-300">Nombre</th>
+                <th className="py-1.5 px-20 font-medium border border-gray-300">Archivo</th>
+                <th className="py-1.5 px-20 font-medium border border-gray-300">Opciones</th>
+              </tr>
+            </thead>
+            <tbody >
+              {files.map((file, index) => (
+                <tr key={file._id} className="border border-gray-300  font-light">
+                  <td className="border border-gray-300 text-[0.65rem] pl-3">{file._id}</td>
+                  <td className="border border-gray-300 text-[0.65rem] pl-3">timestamps</td>
+                  <td className="border border-gray-300 text-[0.65rem] pl-3">{file.content}</td>
+                  <td className="border border-gray-300 text-center ">
+                    <button
+                      onClick={handleprint}
+                      className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] hover:bg-gray-50"
+                    >
+                      Visualizar
+                    </button>
+                  </td>
+                  <td className="border border-gray-300 text-center">
+                    <button
+                      onClick={() => showDeleteAlert(file._id)}
+                      className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] my-2 hover:bg-gray-50"
+                    >
+                      Eliminar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
       {showAlert && (
@@ -107,13 +129,23 @@ const Impresion = () => {
           <div className="bg-white p-5 rounded shadow">
             <p className="text-xl mb-5">¿Estás seguro que deseas eliminar?</p>
             <div className="flex justify-end">
-              <button className="rounded bg-red-500 text-white px-4 py-2 mr-2" onClick={confirmDelete}>ELIMINAR</button>
-              <button className="rounded bg-gray-200 px-4 py-2" onClick={cancelDelete}>CANCELAR</button>
+              <button
+                className="rounded bg-red-500 text-white px-4 py-2 mr-2 "
+                onClick={confirmDelete}
+              >
+                ELIMINAR
+              </button>
+              <button
+                className="rounded bg-gray-200 px-4 py-2"
+                onClick={cancelDelete}
+              >
+                CANCELAR
+              </button>
             </div>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
