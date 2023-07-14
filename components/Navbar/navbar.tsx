da006fa95e716c1f6c3a4c0feb5e89edc42ccefa
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-14 16:34:35 +0200

    meto toggle al menu

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import Cookies from 'js-cookie';
 
 const Navbar: React.FC = () => {
   const [selectedButton, setSelectedButton] = useState("Notificaciones");
@@ -10,55 +9,35 @@ const Navbar: React.FC = () => {
     const currentPath = router.pathname;
     if (currentPath === "/dashboard") {
       setSelectedButton("HOME");
-    } else if (currentPath === "/dashboard/impresion" || currentPath === "/dashboard/impresion" ) {
+    } else if (
+      currentPath === "/dashboard/impresion" ||
+      currentPath === "/dashboard/impresion"
+    ) {
       setSelectedButton("impresion");
-    } else if (currentPath === "/dashboard/opcion2" || currentPath === "/dashboard/opcion2" ) {
+    } else if (
+      currentPath === "/dashboard/opcion2" ||
+      currentPath === "/dashboard/opcion2"
+    ) {
       setSelectedButton("opcion2");
     }
   }, [router.pathname]);
 
-  const handleLogout = async () => {
-    Cookies.remove('authvalue');
-    try {
-      const res = await fetch("http://localhost:5000/auth/logout", {
-        method: "POST",
-        credentials: "include", // Para enviar las cookies al servidor
-      });
-      if (res.status === 200) {
-        // Se ha cerrado sesión con éxito
-        router.push("/login"); // Redirige al usuario a la página de inicio de sesión
-      } else {
-        // Manejar el caso de error si no se pudo cerrar sesión
-        console.error("No se pudo cerrar sesión");
-      }
-    } catch (error) {
-      console.error("Error al cerrar sesión", error);
-    }
-    router.push("/");
-  };
+  
 
   return (
-    <nav className=" bg-cyan-950 bg-opacity-60 backdrop-filter backdrop-blur-lg flex items-center justify-between px-4 py-2 md:text-xs xl:text-base">
-      <button
-        className={`bg-opacity-25 text-white rounded-full py-2 px-3 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer ${
-          selectedButton === "HOME" ? "bg-white bg-opacity-50 text-cyan-950" : ""
-        }`}
-        onClick={() => router.push("/dashboard/impresion")}
-        >
-          Impresión
-      </button>
-      <div>
- 
- 
-         
-        <button style={{fontSize:'0.60rem'}}
-          onClick={handleLogout}
-          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        >
-          Cerrar sesión
-        </button>
-      </div>
-    </nav>
+    <nav className="flex flex-col bg-slate-800  items-start text-start py-2 md:text-xs xl:text-base">
+      <h2
+        className="flex flex-wrap bg-opacity-25 text-white py-1 pr-8 pl-4 mr-5 text-md mt-3 font-semibold  "
+       >
+        Tótem interactivo MAD
+      </h2>
+          <h2 className="text-gray-400 text-xs pl-6 mt-7">Impresión de recuerdos</h2>
+          <div className="flex flex-row text-white text-xs    mt-2 bg-gray-50 w-full justify-left bg-opacity-30">
+            <button className="bg-white pl-1"></button>
+            <button className="flex text-white text-xs  pl-8 py-2   bg-gray-50 w-full justify-left bg-opacity-30">Dioses del Olimpo</button>
+          </div>
+
+     </nav>
   );
 };
 
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,15 +1,282 @@
-// Detallenotificacion.js
+import React, { useState, useEffect, useRef } from "react";
 import Navbar from "../../components/Navbar/navbar";
-import Link from 'next/link'
+import { useReactToPrint } from "react-to-print";
+import Image from "next/image";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+
+interface File {
+  _id: string;
+  nombre: string;
+  midios: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
 const Dashboard = () => {
+  const [files, setFiles] = useState<File[]>([]);
+  const [showAlert, setShowAlert] = useState(false); // Estado para mostrar/ocultar la alerta
+  const [selectedFileId, setSelectedFileId] = useState(""); // Estado para almacenar el ID del archivo seleccionado
+  const componentRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const [navbarVisible, setNavbarVisible] = useState(true);
+ 
+  const handleToggleNavbar = () => {
+    setNavbarVisible(!navbarVisible);
+   };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/files/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Credenciales incorrectas");
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        setFiles(response);
+      })
+      .catch((error) => {
+        console.error("Ha ocurrido un error:", error);
+      });
+  }, []);
+
+  const handleDelete = (id: string) => {
+    fetch(`http://localhost:5000/files/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          window.location.reload();
+        } else {
+          throw new Error("Error al eliminar el archivo");
+        }
+      })
+      .catch((error) => {
+        console.error("Ha ocurrido un error:", error);
+      });
+  };
+
+  const showDeleteAlert = (id: string) => {
+    setSelectedFileId(id);
+    setShowAlert(true);
+  };
+
+  const cancelDelete = () => {
+    setShowAlert(false);
+  };
+
+  const confirmDelete = () => {
+    setShowAlert(false);
+    handleDelete(selectedFileId);
+  };
+  const handleLogout = async () => {
+    Cookies.remove("authvalue");
+    try {
+      const res = await fetch("http://localhost:5000/auth/logout", {
+        method: "POST",
+        credentials: "include", // Para enviar las cookies al servidor
+      });
+      if (res.status === 200) {
+        // Se ha cerrado sesión con éxito
+        router.push("/login"); // Redirige al usuario a la página de inicio de sesión
+      } else {
+        // Manejar el caso de error si no se pudo cerrar sesión
+        console.error("No se pudo cerrar sesión");
+      }
+    } catch (error) {
+      console.error("Error al cerrar sesión", error);
+    }
+    router.push("/");
+  };
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    pageStyle: "@page { size: landscape; }",
+  });
+
+  const contenidodinámico = "CONTENIDO";
+  const contenidodinámico2 = "PARA";
+
   return (
-    <>
-    <Navbar />
-        <h1 className="mt-20 text-2xl">
-            Check home
-        </h1>
-    </>
-    );
+    <div className="flex h-screen w-screen bg-gray-100">
+      {navbarVisible && <Navbar />}
+      <div className="flex flex-col w-screen shadow">
+        <nav className="flex flex-row bg-white justify-between">
+          <div className="px-2 py-1">
+            <button onClick={handleToggleNavbar}>
+                        <Image
+              src="/menuicon.png"
+              alt="menuicon"
+              width={15}
+              height={15}
+            />
+            </button>
+          </div>
+          <div className="px-2 py-1">
+            <button
+              style={{ fontSize: "0.60rem" }}
+              onClick={handleLogout}
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+            >
+              Cerrar sesión
+            </button>
+          </div>
+        </nav>
+        <div className="p-5">
+            <h2 className="mb-4 ml-3 text-lg">Juego Dioses del Olimpo  </h2>
+          <div className="mr-20 bg-white p-5 ">
+            <div>
+              //HERE
+            </div>
+            <table className="text-xs border border-gray-300 bg-white w-full text-left">
+              <thead>
+                <tr className="border border-gray-300">
+                  <th className="py-1.5  font-medium border border-gray-300">
+                    Hora
+                  </th>
+                  <th className="py-1.5  font-medium border border-gray-300">
+                    Nombre
+                  </th>
+                  <th className="py-1.5 font-medium border border-gray-300">
+                    Dios
+                  </th>
+                  <th className="py-1.5  font-medium border border-gray-300">
+                    Archivo
+                  </th>
+                  <th className="py-1.5   font-medium border border-gray-300">
+                    Opciones
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {files.map((file, index) => (
+                  <tr
+                    key={file._id}
+                    className="border border-gray-300 font-light"
+                  >
+                    <td className="border border-gray-300 text-[0.65rem] text-center">
+                      {new Date(file.updatedAt).toLocaleTimeString()} -{" "}
+                      {new Date(file.updatedAt).toLocaleDateString()}
+                    </td>{" "}
+                    <td className="border border-gray-300 text-[0.65rem] text-center">
+                      {file.nombre}
+                    </td>
+                    <td className="border border-gray-300 text-[0.65rem] text-center">
+                      {file.midios}
+                    </td>
+                    <td className="border border-gray-300 text-center">
+                      <button
+                        onClick={handlePrint}
+                        className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] hover:bg-gray-50"
+                      >
+                        Visualizar
+                      </button>
+                    </td>
+                    <td className="border border-gray-300 text-center">
+                      <button
+                        onClick={() => showDeleteAlert(file._id)}
+                        className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] my-2 hover:bg-gray-50"
+                      >
+                        Eliminar
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      {showAlert && (
+        <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-gray-500 bg-opacity-75">
+          <div className="bg-white p-5 rounded shadow">
+            <p className="text-xl mb-5">¿Estás seguro que deseas eliminar?</p>
+            <div className="flex justify-end">
+              <button
+                className="rounded bg-red-500 text-white px-4 py-2 mr-2"
+                onClick={confirmDelete}
+              >
+                ELIMINAR
+              </button>
+              <button
+                className="rounded bg-gray-200 px-4 py-2"
+                onClick={cancelDelete}
+              >
+                CANCELAR
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      <div style={{ display: "none" }}>
+        <ComponentToPrint
+          ref={componentRef}
+          nombre={files.length > 0 ? files[0].nombre : ""}
+          tuDios={files.length > 0 ? files[0].midios : ""}
+          contenidodinámico={contenidodinámico}
+          contenidodinámico2={contenidodinámico2}
+        />
+      </div>
+    </div>
+  );
 };
+
+const ComponentToPrint = React.forwardRef(function ComponentToPrint(
+  {
+    nombre,
+    tuDios,
+    contenidodinámico,
+    contenidodinámico2,
+  }: {
+    nombre: string;
+    tuDios: string;
+    contenidodinámico: any;
+    contenidodinámico2: any;
+  },
+  ref: React.Ref<HTMLDivElement>
+) {
+  return (
+    <div ref={ref}>
+      <div className="flex flex-col mb-20 text-center justify-center m-20">
+        <h1 className="text-3xl mt-20">{nombre}</h1>
+        <p className="text-black text-xs">TU DIOS ES</p>
+        <div className="flex flex-row" style={{ height: "30", width: "40" }}>
+          <div className="flex flex-col items-center" style={{ flex: 1 }}>
+            <Image
+              src="/midiosfoto.png"
+              alt="midiosfoto"
+              width={120}
+              height={120}
+            />
+          </div>
+          <div className="flex flex-col p-5 text-black" style={{ flex: 1 }}>
+            <div className="flex flex-col p-2">
+              <h2 className="font-bold text-xl">{tuDios}</h2>
+              <div className="text-xs">
+                <p>{contenidodinámico}</p>
+                <p>{contenidodinámico2}</p>
+                <p> EL DIOS {tuDios}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+ComponentToPrint.displayName = "ComponentToPrint";
 
 export default Dashboard;
