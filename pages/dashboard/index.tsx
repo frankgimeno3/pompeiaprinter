9c0b1fe78ade66941c1dccfb1ea245453c3bbfcd
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-22 10:10:00 +0200

    añado ordenacion tabla

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,6 +4,8 @@ import Whitenav from "../../components/Navbar/Whitenav";
 import { useReactToPrint } from "react-to-print";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import Contenido from "../../lib/slogans.json";
+import Contenidoeng from "../../lib/sloganseng.json";
 
 interface File {
   _id: string;
@@ -12,17 +14,20 @@ interface File {
   createdAt: string;
   updatedAt: string;
   __v: number;
+  lang:string;
 }
 
 const Dashboard = () => {
   const [files, setFiles] = useState<File[]>([]);
-  const [showAlert, setShowAlert] = useState(false); // Estado para mostrar/ocultar la alerta
-  const [selectedFileId, setSelectedFileId] = useState(""); // Estado para almacenar el ID del archivo seleccionado
+  const [showAlert, setShowAlert] = useState(false);
+  const [selectedFileId, setSelectedFileId] = useState("");
   const componentRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const [showMoreRows, setShowMoreRows] = useState(false);
   const maxRowsToShow = 10;
   const [navbarVisible, setNavbarVisible] = useState(true);
+  const [contenidodios, setcontenidodios] = useState(true);
+  const [currentOrder, setCurrentOrder] = useState<string>("");
 
   useEffect(() => {
     fetch("http://localhost:5000/files/", {
@@ -86,10 +91,30 @@ const Dashboard = () => {
     pageStyle: "@page { size: landscape; }",
   });
 
-  const contenidodinámico = "CONTENIDO";
-  const contenidodinámico2 = "PARA";
   const handleShowMoreRows = () => {
     setShowMoreRows(true);
+  };
+
+  const handleHeaderClick = (header: string) => {
+    const newOrder = header === currentOrder ? `-${header}` : header;
+    const sortedTableData = files.slice().sort((a, b) => {
+      if (header === 'updatedAt') {
+        // Ordenar por fecha y hora al seleccionar el cabezal Hora
+        return (
+          new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime()
+        );
+      } else {
+        // Ordenar alfanuméricamente para los cabezales Nombre y Dios
+        return (a as any)[header].localeCompare((b as any)[header]);
+      }
+    });
+
+    if (newOrder.startsWith('-')) {
+      sortedTableData.reverse();
+    }
+
+    setFiles(sortedTableData);
+    setCurrentOrder(newOrder);
   };
 
   return (
@@ -100,64 +125,59 @@ const Dashboard = () => {
         <div className="p-5">
           <h2 className="mb-4 ml-3 text-lg">Juego Dioses del Olimpo </h2>
           <div className="mr-20 bg-white p-5 ">
-            <table className="text-xs border border-gray-300 bg-white w-full text-left">
-              <thead>
-                <tr className="border border-gray-300">
-                  <th className="py-1.5 pl-5  font-medium border border-gray-300">
-                    Hora
-                  </th>
-                  <th className="py-1.5 pl-5  font-medium border border-gray-300">
-                    Nombre
-                  </th>
-                  <th className="py-1.5 pl-5 font-medium border border-gray-300">
-                    Dios
-                  </th>
-                  <th className="py-1.5 text-center font-medium border border-gray-300">
-                    Archivo
-                  </th>
-                  <th className="py-1.5   text-center  font-medium border border-gray-300">
-                    Opciones
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {files
-                  .slice(0, showMoreRows ? files.length : maxRowsToShow)
-                  .map((file, index) => (
-                    <tr
-                      key={file._id}
-                      className="border border-gray-300 font-light"
-                    >
-                      <td className="border border-gray-300 text-[0.65rem] text-left pl-5">
-                        {new Date(file.updatedAt).toLocaleTimeString()} -{" "}
-                        {new Date(file.updatedAt).toLocaleDateString()}
-                      </td>{" "}
-                      <td className="border border-gray-300 text-[0.65rem] text-left pl-5">
-                        {file.nombre}
-                      </td>
-                      <td className="border border-gray-300 text-[0.65rem] text-left pl-5">
-                        {file.midios}
-                      </td>
-                      <td className="border border-gray-300 text-center">
-                        <button
-                          onClick={handlePrint}
-                          className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] hover:bg-gray-50"
-                        >
-                          Visualizar
-                        </button>
-                      </td>
-                      <td className="border border-gray-300 text-center">
-                        <button
-                          onClick={() => showDeleteAlert(file._id)}
-                          className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] my-2 hover:bg-gray-50"
-                        >
-                          Eliminar
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
+          <table className="text-xs border border-gray-300 bg-white w-full text-left">
+  <thead>
+    <tr className="border border-gray-300">
+      <th className="py-1.5 pl-5 font-medium border border-gray-300 cursor-pointer" onClick={() => handleHeaderClick('updatedAt')}>
+        Hora
+      </th>
+      <th className="py-1.5 pl-5 font-medium border border-gray-300 cursor-pointer" onClick={() => handleHeaderClick('nombre')}>
+        Nombre
+      </th>
+      <th className="py-1.5 pl-5 font-medium border border-gray-300 cursor-pointer" onClick={() => handleHeaderClick('midios')}>
+        Dios
+      </th>
+      <th className="py-1.5 text-center font-medium border border-gray-300">
+        Archivo
+      </th>
+      <th className="py-1.5 text-center font-medium border border-gray-300">
+        Opciones
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    {files.map((file) => (
+      <tr key={file._id} className="border border-gray-300 font-light">
+        <td className="border border-gray-300 text-[0.65rem] text-left pl-5">
+          {new Date(file.updatedAt).toLocaleDateString()} -{" "}
+          {new Date(file.updatedAt).toLocaleTimeString()} 
+        </td>
+        <td className="border border-gray-300 text-[0.65rem] text-left pl-5">
+          {file.nombre}
+        </td>
+        <td className="border border-gray-300 text-[0.65rem] text-left pl-5">
+          {file.midios}
+        </td>
+        <td className="border border-gray-300 text-center">
+          <button
+            className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] hover:bg-gray-50 btn-visualizar"
+            onClick={handlePrint}
+          >
+            Visualizar
+          </button>
+        </td>
+        <td className="border border-gray-300 text-center">
+          <button
+            className="rounded bg-gray-100 shadow px-5 py-1 text-xs text-[0.60rem] my-2 hover:bg-gray-50 btn-eliminar"
+            onClick={() => showDeleteAlert(file._id)}
+          >
+            Eliminar
+          </button>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</table>
             {files.length > maxRowsToShow && !showMoreRows && (
               <div className="flex justify-center mt-4">
                 <button
@@ -197,8 +217,9 @@ const Dashboard = () => {
           ref={componentRef}
           nombre={files.length > 0 ? files[0].nombre : ""}
           tuDios={files.length > 0 ? files[0].midios : ""}
-          contenidodinámico={contenidodinámico}
-          contenidodinámico2={contenidodinámico2}
+          contenido ={Contenido}
+          contenidoeng={Contenidoeng}
+          // lang={files[0].lang}
         />
       </div>
     </div>
@@ -209,36 +230,53 @@ const ComponentToPrint = React.forwardRef(function ComponentToPrint(
   {
     nombre,
     tuDios,
-    contenidodinámico,
-    contenidodinámico2,
+    contenido,
+    contenidoeng,
   }: {
     nombre: string;
     tuDios: string;
-    contenidodinámico: any;
-    contenidodinámico2: any;
+    contenido: any;
+    contenidoeng: any;
   },
   ref: React.Ref<HTMLDivElement>
 ) {
+  const imagendios = `/dioses/${tuDios}.png`;
+
   return (
-    <div ref={ref}>
-      <div className="flex flex-col mb-20 text-center justify-center m-20">
-        <h1 className="text-3xl mt-20">{nombre}</h1>
-        <p className="text-black text-xs">TU DIOS ES</p>
-        <div className="flex flex-row" style={{ height: "30", width: "40" }}>
-          <div className="flex flex-col items-center" style={{ flex: 1 }}>
-            <Image
-              src="/midiosfoto.png"
-              alt="midiosfoto"
-              width={120}
-              height={120}
-            />
+    <div
+      ref={ref}
+      className="h-screen flex justify-center text-center relative"
+    >
+      {/* Contenedor para la imagen de fondo */}
+      <div
+        className="absolute top-0 left-0 w-full h-full z-0"
+        style={{ opacity: 0.3 }} // Ajustar la opacidad según sea necesario
+      >
+        <img
+          src="/fondo1.png"
+          alt="Fondo"
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </div>
+      {/* Contenido del componente */}
+      <div className="z-10 mt-24 pt-24">
+        <div className="flex flex-row text-center justify-center align-center  px-24 mx-14">
+          <div className="flex-1 flex items-center justify-center pl-5">
+            <Image src={imagendios} alt={tuDios} width={200} height={200} />
           </div>
-          <div className="flex flex-col p-5 text-black" style={{ flex: 1 }}>
-            <div className="flex flex-col p-2">
-              <h2 className="font-bold text-xl">{tuDios}</h2>
+
+          <div className="flex-1 flex flex-col  align-center ">
+            <h1 className="text-6xl mt-10 ">{nombre}</h1>
+            <p className="text-black text-lg mt-5   text-black">TU DIOS ES</p>
+            <h2 className="  text-4xl  mb-5   ">{tuDios}</h2>
+            <div className="text-black">
+              {/* <div className="text-lg mb-10 px-10">{Contenido[tuDios]}</div> */}
               <div className="text-xs">
-                <p>{contenidodinámico}</p>
-                <p>{contenidodinámico2}</p>
+                {/* <p>{contenidodinámico}</p>
+                <p>{contenidodinámico2}</p> */}
                 <p> EL DIOS {tuDios}</p>
               </div>
             </div>
