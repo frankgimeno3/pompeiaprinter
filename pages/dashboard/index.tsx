52522f55cd3421c00bf0bc79f8d437862615a430
Frank Gimeno <frankgimeno3@gmail.com>
2023-09-05 20:04:00 +0200

    solucionado tema fotos

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -381,43 +381,40 @@ const ComponentToPrint = React.forwardRef(function ComponentToPrint(
         ref={ref}
         className="h-screen flex justify-center text-center relative cinzel-font text-yellow-900 text-opacity-50 font-light"
       >
-        <div
-          className="absolute top-0 left-0 w-full h-full z-0"
-          style={{ opacity: 0.3 }}
-        >
+        <div className="flex flex-row text-left justify-center align-center  px-20 pr-24 mx-14"   style={{ zIndex: 1 }}>
+          <div className="flex-1 flex flex-col  pl-14 ml-12 py-24 pr-2 justify-left ">
+            <h1 className="text-5xl  pt-24 ">{nombre}</h1>
+            <p className="text-black text-2xl mt-2 mb-8   text-black">
+              {tudioses}
+            </p>
+            <h2 className="text-black  text-5xl  mb-5   ">{tuDios}</h2>
+            <div className="text-black">
+              <div className="text-xl mb-10 pr-5">{contenidoprint}</div>
+            </div>
+          </div>
+          <div className="flex-2 flex items-center justify-center   ">
+            <img
+              src={imagendios}
+              alt={tuDios}
+              width={350}
+              height={350}
+               className=" pt-14 mr-10"
+            />
+          </div>
+        </div>
+
+        <div className="absolute top-0 left-0 w-full h-full z-0">
           <img
-            src="/fondo2.png"
-            alt="Fondo"
+            src="/PortfolioDiseno.png"
+            alt="PortfolioDiseno"
             style={{
               width: "100%",
               height: "100%",
             }}
           />
         </div>
-        <div className="flex flex-row text-left justify-center align-center  px-20 pr-24 mx-14">
-             <div className="flex-1 flex flex-col  pl-14 ml-12 py-24 pr-2 justify-left ">
-              <h1 className="text-5xl  pt-24 ">{nombre}</h1>
-              <p className="text-black text-2xl mt-2 mb-8   text-black">
-                {tudioses}
-              </p>
-              <h2 className="text-black  text-5xl  mb-5   ">{tuDios}</h2>
-              <div className="text-black">
-                <div className="text-xl mb-10 pr-5">{contenidoprint}</div>
-              </div>
-            </div>
-            <div className="flex-2 flex items-center justify-center   ">
-              <Image
-                src={imagendios}
-                alt={tuDios}
-                width={400}
-                height={400}
-                style={{ width: "auto", height: "auto" }}
-              className=" pt-14 mr-10" 
-              />
-            </div>
-          </div>
-        </div>
-     </>
+      </div>
+    </>
   );
 });
 
