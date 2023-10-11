ca9cac9e2d264216b7ca080eff4cf1022fe40798
frank gimeno <frankgimeno3@gmail.com>
2023-10-11 11:09:55 +0200

    solucion en es

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -370,7 +370,7 @@ const ComponentToPrint = React.forwardRef(function ComponentToPrint(
   }
 
   return (
-    <>
+    <div  >
       <style>
         {`
           @import url('https://fonts.googleapis.com/css2?family=Cinzel&display=swap');
@@ -379,17 +379,17 @@ const ComponentToPrint = React.forwardRef(function ComponentToPrint(
       </style>
       <div
         ref={ref}
-        className="h-screen flex justify-center text-center relative cinzel-font text-yellow-900 text-opacity-50 font-light"
+        className=" h-screen flex justify-center text-center relative cinzel-font text-yellow-900 text-opacity-50 font-light"
       >
         <div className="flex flex-row text-left justify-center align-center  px-20 pr-24 mx-14"   style={{ zIndex: 1 }}>
-          <div className="flex-1 flex flex-col  pl-14 ml-12 py-24 pr-2 justify-left ">
+          <div className="flex-1 flex flex-col  pl-14 ml-12 py-20 pr-2 justify-left ">
             <h1 className="text-5xl  pt-24 ">{nombre}</h1>
             <p className="text-black text-2xl mt-2 mb-8   text-black">
               {tudioses}
             </p>
             <h2 className="text-black  text-5xl  mb-4   ">{tuDios}</h2>
             <div className="text-black">
-              <div className="text-xl mb-10 pr-4">{contenidoprint}</div>
+              <div className="text-lg mb-10 pr-5">{contenidoprint}</div>
             </div>
           </div>
           <div className="flex-2 flex items-center justify-center   ">
@@ -414,7 +414,7 @@ const ComponentToPrint = React.forwardRef(function ComponentToPrint(
           />
         </div>
       </div>
-    </>
+    </div>
   );
 });
 
